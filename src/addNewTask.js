294a8b551233f00a121bc60e90a6c591fd7694ed
shinymayo <138251809+shinymayo@users.noreply.github.com>
2024-06-18 20:53:52 +0100

    Create a pop up form
    Began creating the pop up form. Completed the
    title and description inputs. The form is still
    visible for the ease of development purposes. At
    the later stage it will be hidden

---
+++ src/addNewTask.js
@@ -0,0 +1,7 @@
+function addNewTask() {
+    // needs to show a pop-up box to fill in
+    const overlay = document.getElementById("popupOverlay");
+    overlay.classList.toggle("show");
+}
+
+export default addNewTask;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './style.css';
 
 import createTaskFolders from "./taskFolders";
 import createDefaultTask from "./taskExample";
+import addNewTask from "./addNewTask";
 
 // function component() {
 //   const element = document.createElement("div");
@@ -33,6 +34,60 @@ function createPageItems() {
     mainDiv.classList.add("page");
     document.body.appendChild(mainDiv);
 
+    // popup form overlay to hide it
+    const popupOverlay  = document.createElement("div");
+    popupOverlay.setAttribute("id", "popupOverlay");
+    popupOverlay.classList.add("overlay-container");
+    mainDiv.appendChild(popupOverlay);
+    // pop up box with form
+    const popupBox = document.createElement("div");
+    popupBox.classList.add("popup-box");
+    popupOverlay.appendChild(popupBox);
+    // heading for form
+    const popupBoxHeading = document.createElement("h2");
+    popupBoxHeading.textContent = "New Task Form";
+    popupBoxHeading.style.color = "green";
+    popupBox.appendChild(popupBoxHeading);
+    // the form itself
+    const popupForm = document.createElement("form");
+    popupForm.classList.add("form-container");
+    popupBox.appendChild(popupForm);
+    // ask for title
+    const taskTitleLabel = document.createElement("label");
+    taskTitleLabel.setAttribute("for", "title");
+    taskTitleLabel.classList.add("form-label");
+    taskTitleLabel.textContent = "Title:";
+    popupForm.appendChild(taskTitleLabel);
+    // input field for title
+    const taskTitleInput = document.createElement("input");
+    taskTitleInput.classList.add("form-input");
+    taskTitleInput.setAttribute("type", "text");
+    taskTitleInput.setAttribute("placeholder", "Type title here");
+    taskTitleInput.setAttribute("id", "title");
+    taskTitleInput.setAttribute("name", "title");
+    popupForm.appendChild(taskTitleInput);
+    // const taskDescription
+    const taskDescriptionLabel = document.createElement("label");
+    taskDescriptionLabel.setAttribute("for", "description");
+    taskDescriptionLabel.classList.add("form-label");
+    taskDescriptionLabel.textContent = "Description:";
+    popupForm.appendChild(taskDescriptionLabel);
+    // input field for description
+    const taskDescriptionInput = document.createElement("input");
+    taskDescriptionInput.classList.add("form-input");
+    taskDescriptionInput.setAttribute("type", "text");
+    taskDescriptionInput.setAttribute("placeholder", "Type description here");
+    taskDescriptionInput.setAttribute("id", "description");
+    taskDescriptionInput.setAttribute("name", "description");
+    popupForm.appendChild(taskDescriptionInput);
+    // const taskDueDate
+    // const taskPriority
+    // const addTaskButton
+
+
+
+
+
     // top div
     const topDiv = document.createElement("div");
     topDiv.classList.add("top");
@@ -43,6 +98,7 @@ function createPageItems() {
     const addNewButton = document.createElement("button");
     addNewButton.classList.add("add-new-button");
     addNewButton.textContent = "ADD TASK";
+    addNewButton.addEventListener("click", addNewTask);
     topDiv.appendChild(addNewButton);
 
     // side div
