f273fce15e971e6a3176e88d8528be66b2fe817f
shinymayo <138251809+shinymayo@users.noreply.github.com>
2024-06-18 21:20:10 +0100

    Fix typo

--- src/index.js
+++ src/index.js
@@ -29,107 +29,114 @@ import addNewTask from "./addNewTask";
 // document.body.appendChild(component());
 
 function createPageItems() {
-    // main div
-    const mainDiv = document.createElement("div");
-    mainDiv.classList.add("page");
-    document.body.appendChild(mainDiv);
+  // main div
+  const mainDiv = document.createElement("div");
+  mainDiv.classList.add("page");
+  document.body.appendChild(mainDiv);
 
-    // popup form overlay to hide it
-    const popupOverlay  = document.createElement("div");
-    popupOverlay.setAttribute("id", "popupOverlay");
-    popupOverlay.classList.add("overlay-container");
-    mainDiv.appendChild(popupOverlay);
-    // pop up box with form
-    const popupBox = document.createElement("div");
-    popupBox.classList.add("popup-box");
-    popupOverlay.appendChild(popupBox);
-    // heading for form
-    const popupBoxHeading = document.createElement("h2");
-    popupBoxHeading.textContent = "New Task Form";
-    popupBoxHeading.style.color = "green";
-    popupBox.appendChild(popupBoxHeading);
-    // the form itself
-    const popupForm = document.createElement("form");
-    popupForm.classList.add("form-container");
-    popupBox.appendChild(popupForm);
-    // ask for title
-    const taskTitleLabel = document.createElement("label");
-    taskTitleLabel.setAttribute("for", "title");
-    taskTitleLabel.classList.add("form-label");
-    taskTitleLabel.textContent = "Title:";
-    popupForm.appendChild(taskTitleLabel);
-    // input field for title
-    const taskTitleInput = document.createElement("input");
-    taskTitleInput.classList.add("form-input");
-    taskTitleInput.setAttribute("type", "text");
-    taskTitleInput.setAttribute("placeholder", "Type title here");
-    taskTitleInput.setAttribute("id", "title");
-    taskTitleInput.setAttribute("name", "title");
-    popupForm.appendChild(taskTitleInput);
-    // const taskDescription
-    const taskDescriptionLabel = document.createElement("label");
-    taskDescriptionLabel.setAttribute("for", "description");
-    taskDescriptionLabel.classList.add("form-label");
-    taskDescriptionLabel.textContent = "Description:";
-    popupForm.appendChild(taskDescriptionLabel);
-    // input field for description
-    const taskDescriptionInput = document.createElement("input");
-    taskDescriptionInput.classList.add("form-input");
-    taskDescriptionInput.setAttribute("type", "text");
-    taskDescriptionInput.setAttribute("placeholder", "Type description here");
-    taskDescriptionInput.setAttribute("id", "description");
-    taskDescriptionInput.setAttribute("name", "description");
-    popupForm.appendChild(taskDescriptionInput);
-    // const taskDueDate
-    const taskDueDateLabel = document.createElement("label");
-    taskDueDateLabel.setAttribute("for", "due-date");
-    taskDueDateLabel.classList.add("form-label");
-    taskDescriptionLabel.textContent = "Due Date:";
-    popupForm.appendChild(taskDueDateLabel);
-    // input field for due date
-    const taskDueDateInput = document.createElement("input");
-    taskDueDateInput.classList.add("form-input");
-    taskDueDateInput.setAttribute("type", "datetime-local");
-    taskDueDateInput.setAttribute("id", "due-date");
-    taskDescriptionInput.setAttribute("name", "due-date");
-    popupForm.appendChild(taskDueDateInput);
-    // const taskPriority
-    const taskPriorityLabel = document.createElement("label");
-    taskPriorityLabel.setAttribute("for", "priority-picker");
-    taskPriorityLabel.classList.add("form-label");
-    taskPriorityLabel.textContent = "Choose priority:";
-    popupForm.appendChild(taskPriorityLabel);
-    // const addTaskButton
+  // popup form overlay to hide it
+  const popupOverlay = document.createElement("div");
+  popupOverlay.setAttribute("id", "popupOverlay");
+  popupOverlay.classList.add("overlay-container");
+  mainDiv.appendChild(popupOverlay);
+  // pop up box with form
+  const popupBox = document.createElement("div");
+  popupBox.classList.add("popup-box");
+  popupOverlay.appendChild(popupBox);
+  // heading for form
+  const popupBoxHeading = document.createElement("h2");
+  popupBoxHeading.textContent = "New Task Form";
+  popupBoxHeading.style.color = "green";
+  popupBox.appendChild(popupBoxHeading);
+  // the form itself
+  const popupForm = document.createElement("form");
+  popupForm.classList.add("form-container");
+  popupBox.appendChild(popupForm);
+  // ask for title
+  const taskTitleLabel = document.createElement("label");
+  taskTitleLabel.setAttribute("for", "title");
+  taskTitleLabel.classList.add("form-label");
+  taskTitleLabel.textContent = "Title:";
+  popupForm.appendChild(taskTitleLabel);
+  // input field for title
+  const taskTitleInput = document.createElement("input");
+  taskTitleInput.classList.add("form-input");
+  taskTitleInput.setAttribute("type", "text");
+  taskTitleInput.setAttribute("placeholder", "Type title here");
+  taskTitleInput.setAttribute("id", "title");
+  taskTitleInput.setAttribute("name", "title");
+  popupForm.appendChild(taskTitleInput);
+  // const taskDescription
+  const taskDescriptionLabel = document.createElement("label");
+  taskDescriptionLabel.setAttribute("for", "description");
+  taskDescriptionLabel.classList.add("form-label");
+  taskDescriptionLabel.textContent = "Description:";
+  popupForm.appendChild(taskDescriptionLabel);
+  // input field for description
+  const taskDescriptionInput = document.createElement("input");
+  taskDescriptionInput.classList.add("form-input");
+  taskDescriptionInput.setAttribute("type", "text");
+  taskDescriptionInput.setAttribute("placeholder", "Type description here");
+  taskDescriptionInput.setAttribute("id", "description");
+  taskDescriptionInput.setAttribute("name", "description");
+  popupForm.appendChild(taskDescriptionInput);
+  // const taskDueDate
+  const taskDueDateLabel = document.createElement("label");
+  taskDueDateLabel.setAttribute("for", "due-date");
+  taskDueDateLabel.classList.add("form-label");
+  taskDescriptionLabel.textContent = "Due Date:";
+  popupForm.appendChild(taskDueDateLabel);
+  // input field for due date
+  const taskDueDateInput = document.createElement("input");
+  taskDueDateInput.classList.add("form-input");
+  taskDueDateInput.setAttribute("type", "datetime-local");
+  taskDueDateInput.setAttribute("id", "due-date");
+  taskDescriptionInput.setAttribute("name", "due-date");
+  popupForm.appendChild(taskDueDateInput);
+  // const taskPriority
+  const taskPriorityLabel = document.createElement("label");
+  taskPriorityLabel.setAttribute("for", "priority-picker");
+  taskPriorityLabel.classList.add("form-label");
+  taskPriorityLabel.textContent = "Choose priority:";
+  popupForm.appendChild(taskPriorityLabel);
+  // task priority input low
+  const taskPriorityInput = document.createElement("input");
+  taskPriorityInput.classList.add("form-input");
+  taskPriorityInput.classList.add("priority-low");
+  taskPriorityInput.setAttribute("type", "radio");
+  taskPriorityInput.setAttribute("value", "low");
+  taskPriorityInput.setAttribute("id", "priority-picker");
+  popupForm.appendChild(taskPriorityInput);
+  // task priority input medium
+  // task priority input high
 
+  // const taskAddButton // submit button
 
+  // top div
+  const topDiv = document.createElement("div");
+  topDiv.classList.add("top");
+  topDiv.textContent = "To Do List";
+  mainDiv.appendChild(topDiv);
 
+  // add new button
+  const addNewButton = document.createElement("button");
+  addNewButton.classList.add("add-new-button");
+  addNewButton.textContent = "ADD TASK";
+  addNewButton.addEventListener("click", addNewTask);
+  topDiv.appendChild(addNewButton);
 
+  // side div
+  const sideDiv = document.createElement("div");
+  sideDiv.classList.add("side");
+  // sideDiv.textContent = "Projects";
+  mainDiv.appendChild(sideDiv);
+  createTaskFolders();
 
-    // top div
-    const topDiv = document.createElement("div");
-    topDiv.classList.add("top");
-    topDiv.textContent = "To Do List";
-    mainDiv.appendChild(topDiv);
-
-    // add new button
-    const addNewButton = document.createElement("button");
-    addNewButton.classList.add("add-new-button");
-    addNewButton.textContent = "ADD TASK";
-    addNewButton.addEventListener("click", addNewTask);
-    topDiv.appendChild(addNewButton);
-
-    // side div
-    const sideDiv = document.createElement("div");
-    sideDiv.classList.add("side");
-    // sideDiv.textContent = "Projects";
-    mainDiv.appendChild(sideDiv);
-    createTaskFolders();
-
-    //  tasks div
-    const tasksDiv = document.createElement("div");
-    tasksDiv.classList.add("tasks");
-    mainDiv.appendChild(tasksDiv);
-    createDefaultTask();
+  //  tasks div
+  const tasksDiv = document.createElement("div");
+  tasksDiv.classList.add("tasks");
+  mainDiv.appendChild(tasksDiv);
+  createDefaultTask();
 }
 
 document.body.appendChild(createPageItems());
