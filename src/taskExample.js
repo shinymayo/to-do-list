65b7bc820623d5499a1b6504317b32733d8fb6bb
shinymayo <138251809+shinymayo@users.noreply.github.com>
2024-06-18 14:35:45 +0100

    Add icons, date, checkbox
    Added "delete" and "edit" icons. Styled them as
    well. Also the date and checkbox, need to add some
    functionality to them

--- src/taskExample.js
+++ src/taskExample.js
@@ -1,3 +1,6 @@
+import Delete from "./delete.svg";
+import Edit from "./edit.svg";
+
 function createDefaultTask() {
     // get the area for the task listings
     const taskListingArea = document.querySelector(".tasks");
@@ -5,8 +8,8 @@ function createDefaultTask() {
     // create the task box/line
     const taskBox = document.createElement("div");
     taskBox.classList.add("task-listing");
+    taskBox.classList.add("priority-medium");
     taskListingArea.appendChild(taskBox);
-
     // title
     const title = document.createElement("p");
     title.textContent = "do the dishes";
@@ -17,10 +20,31 @@ function createDefaultTask() {
     description.textContent = "kitchen, after dinner";
     description.classList.add("task-listing-description");
     taskBox.appendChild(description);
-    // due date
-    // priority
-    // check
 
+
+    //create div for icons and edits
+    const iconDiv = document.createElement("div");
+    iconDiv.classList.add("icon-div");
+    taskBox.appendChild(iconDiv);
+    // due date
+    const dueDate = document.createElement("div");
+    dueDate.classList.add("due-date");
+    dueDate.textContent = "15th Jul";
+    iconDiv.appendChild(dueDate);
+    // priority -- will be done at creation
+    // check
+    const completionCheck = document.createElement("input");
+    completionCheck.setAttribute("type", "checkbox");
+    completionCheck.setAttribute("id", "checkbox");
+    iconDiv.appendChild(completionCheck);
+    // create delete icon 
+    const editIcon = new Image();
+    editIcon.src = Edit;
+    iconDiv.appendChild(editIcon);
+    // create edit icon
+    const deleteIcon = new Image();
+    deleteIcon.src = Delete;
+    iconDiv.appendChild(deleteIcon);
 }
 
 
